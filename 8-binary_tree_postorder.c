8248ac3ba97eadf7eebfc5cfb9c2164c1ae818dc
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-02-01 22:06:49 +0100

    Complete tasks 6, 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,15 +2,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes through a binary tree through
- * preorder
+ * binary_tree_postorder - goes through a binary tree through
+ * postorder
  * @tree: pointer to root node of tree to be traversed
  * @func: function to be executed
  * Description - traverses a binary tree
  * Return: void
 */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if ((func != NULL) && (tree != NULL))
 		do_traverse(tree, func);
@@ -28,7 +28,7 @@ void do_traverse(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
 		return;
-	(*func)(tree->n);
 	do_traverse(tree->left, func);
 	do_traverse(tree->right, func);
+	(*func)(tree->n);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -39,5 +39,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t leaf(const binary_tree_t *node, size_t leaf_num);
 void do_traverse(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
